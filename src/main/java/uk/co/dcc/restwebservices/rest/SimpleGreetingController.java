4a2440d4ad507266bf82ad55a2f0c996e4781029
James Gill <worldwidejimbo@gmail.com>
2020-04-28 10:20:55 +0100

    working with @pathvariable

--- src/main/java/uk/co/dcc/restwebservices/rest/SimpleGreetingController.java
+++ src/main/java/uk/co/dcc/restwebservices/rest/SimpleGreetingController.java
@@ -1,6 +1,7 @@
 package uk.co.dcc.restwebservices.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import uk.co.dcc.restwebservices.model.GreetingBean;
@@ -8,14 +9,22 @@ import uk.co.dcc.restwebservices.model.GreetingBean;
 @RestController
 public class SimpleGreetingController {
 	
+	// Simple string
 	@GetMapping("/hello-user")
 	public String getGreeting() {
 		return "Hello User!";
 	}
 	
+	// Complex bean
 	@GetMapping("/hello-user-bean")
 	public GreetingBean getGreetingBean() {
 		return new GreetingBean("Welcome, User!");
+	}
+	
+	// Path variable
+	@GetMapping("/hello-user/{name}")
+	public GreetingBean getGreetingBean(@PathVariable String name) {
+		return new GreetingBean(String.format("Welcome, %s", name));
 	}
 
 }
